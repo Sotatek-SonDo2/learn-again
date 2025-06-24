9e647321d6ed743d01b6823da61638de29fb96fa
Sotatek-SonDo2 <son.do2@sotatek.com>
2025-06-24 16:12:29 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-var fullName = "Do xuan son"
-var age = 19;
 
-alert(fullName);
-alert(age);
+        const mangGoc = [1, 2, 3, 4, 5, "a", "b", "c"];
+
+        function daoNguocMang(arr) {
+            const mangMoi = [];
+            
+            for (let i = arr.length - 1; i >= 0; i--) {
+                mangMoi.push(arr[i]);
+            }
+            return mangMoi;
+        }
+
+        const mangDaDaoNguoc = daoNguocMang(mangGoc);
+
+        console.log("Mảng gốc:", mangGoc);
+        console.log("Mảng sau khi đảo ngược:", mangDaDaoNguoc);
+
+        document.getElementById('mang-ban-dau').textContent = JSON.stringify(mangGoc);
+        document.getElementById('mang-ket-qua').textContent = JSON.stringify(mangDaDaoNguoc);
